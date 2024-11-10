ce65d7b0e2c07ac1de2a10989effb0f6c508374b
Jaketus0 <via.angelyca@ti.ukdw.ac.id>
2024-11-10 21:44:36 +0700

    fix: update ImageKit configuration with hardcoded keys and endpoint; comment out environment variables

--- libs/imagekit.js
+++ libs/imagekit.js
@@ -1,9 +1,13 @@
 const ImageKit = require('imagekit');
 
 let imagekit = new ImageKit({
-    publicKey : process.env.IMAGEKIT_PUBLICKEY,
-    privateKey : process.env.IMAGEKIT_PRIVATEKEY,
-    urlEndpoint : process.env.IMAGEKIT_URL
+    // publicKey : process.env.IMAGEKIT_PUBLICKEY,
+    // privateKey : process.env.IMAGEKIT_PRIVATEKEY,
+    // urlEndpoint : process.env.IMAGEKIT_URL
+
+    publicKey : "public_xlYyb9/3+H6ilmw0wPClAxmLuys=",
+    privateKey : "private_AS3/NsSvN8ayLXAI6jCawBbYhd0=",
+    urlEndpoint : "https://ik.imagekit.io/molusxoy/"
 });
 
 module.exports = imagekit;
