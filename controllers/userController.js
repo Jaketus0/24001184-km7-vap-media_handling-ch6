f98097be3ca25caa877914cc4dc3ac8e8a57a538
Jaketus0 <via.angelyca@ti.ukdw.ac.id>
2024-11-10 16:15:57 +0700

    fix: correct import path for imagekit library

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const {PrismaClient} = require("@prisma/client")
 const prisma = new PrismaClient()
-const imagekit = require("../libs/imageKit");
+const imagekit = require("../libs/imagekit");
 
 class UserControllers{ 
     static async addUser(req,res){
